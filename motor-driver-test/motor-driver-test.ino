658fdddfd04697957af1551fbb765ca513b2c7cf
Bryan Vermaat <bryanF1001@users.noreply.github.com>
2019-01-29 11:24:44 +0100

    Updated code to work with multiple steppers

--- motor-driver-test/motor-driver-test.ino
+++ motor-driver-test/motor-driver-test.ino
@@ -1,70 +1,144 @@
-#define dirPin 4 //Pin which toggles the direction
-#define stepPin 3 //Pin that sends the step pulse
+#define dirPin0 2 //Pin which toggles the direction
+#define stepPin0 3 //Pin that sends the step pulse
 
-int microsDelay = 500; //Delay in microseconds for each pulse. Lower = faster speed, but arduino might mess up low delays, and so might the steppermotor.
-int dir = HIGH; //default direction.
-long pos = 0; //we count position
+#define dirPin1 4
+#define stepPin1 5
+
+#define dirPin2 6
+#define stepPin2 9
+
+#define dirPin3 10
+#define stepPin3 11
+
+int minMicrosDelay = 2000;
+int maxMicrosDelay = 600;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Started");
 
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-}
+  pinMode(dirPin0, OUTPUT);
+  pinMode(stepPin0, OUTPUT);
 
-bool doRotate = true;
+  pinMode(dirPin1, OUTPUT);
+  pinMode(stepPin1, OUTPUT);
+
+  pinMode(dirPin2, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+
+  pinMode(dirPin3, OUTPUT);
+  pinMode(stepPin3, OUTPUT);
+}
 
 void loop() {
   //read serial, x = stop, s = start.
   if (Serial.available() > 0) {
     char in = Serial.read();
-
-    //compare the char and toggle doRotate.
-    if (in == 's') { //start rotating
-      Serial.println("doRotate: true");
-      doRotate = true;
-      
-    } else if (in == 'x') { //stop rotating
-      Serial.println("doRotate: false");
-      doRotate = false;
-      
-    } else if (in == 'p'){ //print pos
-      Serial.print("pos: ");
-      Serial.println(pos, DEC);
-    }
+    Serial.println("> '" + String(in) + "'");
+    moveDir(in, 500);
   }
+}
 
-  if (doRotate) {
-    dir = !dir;               //switch direction
-    Serial.print("dir: ");
-    Serial.print(dir?"HIGH":"LOW");
-    Serial.println("\t rotate(200)");
-    rotate(200);              //rotate 200 * 1.8 = 360 degrees.
-    delay(500);               //delay to prevent spam
+void changeDir(int whichMotor, int dir) {
+  switch (whichMotor) {
+    case 0:
+      digitalWrite(dirPin0, dir);
+      break;
+    case 1:
+      digitalWrite(dirPin1, dir);
+      break;
+    case 2:
+      digitalWrite(dirPin2, dir);
+      break;
+    case 3:
+      digitalWrite(dirPin3, dir);
+      break;
+    case 4:
+      digitalWrite(dirPin0, dir);
+      digitalWrite(dirPin1, dir);
+      digitalWrite(dirPin2, dir);
+      digitalWrite(dirPin3, dir);
+      break;
+    default:
+      break;
   }
 }
 
 //steps one step. Be careful, no safety features here.
 void stepMotor(int delayM) {
-  digitalWrite(stepPin, HIGH);
+ // Serial.println(delayM);
+  
+  digitalWrite(stepPin0, HIGH);
+  digitalWrite(stepPin1, HIGH);
+  digitalWrite(stepPin2, HIGH);
+  digitalWrite(stepPin3, HIGH);
   delayMicroseconds(delayM);
-  digitalWrite(stepPin, LOW);
+  digitalWrite(stepPin0, LOW);
+  digitalWrite(stepPin1, LOW);
+  digitalWrite(stepPin2, LOW);
+  digitalWrite(stepPin3, LOW);
   delayMicroseconds(delayM);
 }
 
 //rotate/move  a certain amount of steps.
-void rotate(int steps) {
-  digitalWrite(dirPin, dir);
+void takeSteps(int steps) {
   // Enables the motor to move in a particular direction
   // Makes 200 pulses one full cycle rotation
+  int tenPercent = steps * 0.1;
+  int ninetyPercent = steps * 0.9;
+  
   for (int i = 0; i < steps; i++) {
-    stepMotor(microsDelay);
-    //change position each step.
-    if (dir) {
-      pos++;
+    if(i < tenPercent) {
+      stepMotor(map(i, 0, tenPercent, minMicrosDelay, maxMicrosDelay));
+    } else if(i > ninetyPercent){
+      stepMotor(map(i, ninetyPercent, steps, maxMicrosDelay, minMicrosDelay));    
     } else {
-      pos--;
+      stepMotor(maxMicrosDelay);  
     }
+  }
+}
+
+#define CLOCKWISE HIGH
+#define ANTICLOCKWISE LOW
+
+void moveDir(char whichDir, int steps){
+  switch(whichDir){
+    case 'f':
+      Serial.println("Moving 'f'orward");
+      //move 'f'orward
+      changeDir(4, CLOCKWISE);
+      takeSteps(steps);
+    break;
+    
+    case 'b':
+      Serial.println("Moving 'b'orward");
+      //move 'b'orward
+      changeDir(4, ANTICLOCKWISE);
+      takeSteps(steps);
+    break;
+    
+    case 'r':
+      Serial.println("Moving 'r'orward");
+      //move 'r'orward
+      changeDir(0, ANTICLOCKWISE);
+      changeDir(1, CLOCKWISE);
+      changeDir(2, CLOCKWISE);
+      changeDir(3, ANTICLOCKWISE);
+      takeSteps(steps);
+    break;
+    
+    case 'l':
+      Serial.println("Moving 'l'orward");
+      //move 'l'orward
+      changeDir(0, CLOCKWISE);
+      changeDir(1, ANTICLOCKWISE);
+      changeDir(2, ANTICLOCKWISE);
+      changeDir(3, CLOCKWISE);
+      takeSteps(steps);
+    break;
+    
+    default:
+      Serial.println("Watchu mean");
+    break;
   }
 }
